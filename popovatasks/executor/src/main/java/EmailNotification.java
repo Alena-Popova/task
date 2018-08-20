40fa33c09829fb7b58e10eb90008132569ec6f7a
Alena-Popova <popova-alena1995@mail.ru>
2018-08-20 13:19:55 +0300

    email note

--- popovatasks/executor/src/main/java/EmailNotification.java
+++ popovatasks/executor/src/main/java/EmailNotification.java
@@ -2,25 +2,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification extends ExecutorServicePool {
-    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    String subject;
-    String body;
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     @Override
     void emailTo(User user) {
-        subject = String.format("Notification %s to email %s", user.getName(), user.getEmail());
-        body = String.format("Add a new event to %s", user.getName());
-        pool.submit(new Runnable() {
-            @Override
-            public void run() {
-                send(subject, body, user.getEmail());
-            }
-        });
+        String subject = String.format("Notification %s to email %s", user.getName(), user.getEmail());
+        String body = String.format("Add a new event to %s", user.getName());
+        pool.submit(()-> send(subject, body, user.getEmail()));
     }
 
 
     @Override
-    void send(String suject, String body, String email) {
+    void send(String subject, String body, String email) {
 
     }
 
